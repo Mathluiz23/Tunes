86caf9a8360c36946b3edc1ddb593c4cd429448e
Mathluiz23 <matheus_luizs@hotmail.com>
2026-07-27 22:21:21 -0300

    feat: adiciona tipos de usuario e favoritas

---
+++ src/types/user.ts
@@ -0,0 +1,14 @@
+export interface User {
+  name: string;
+  email: string;
+  description: string;
+  image: string;
+}
+
+export interface FavoriteTrack {
+  trackId: number;
+  trackName: string;
+  artist: string;
+  previewUrl: string;
+  artwork: string;
+}
